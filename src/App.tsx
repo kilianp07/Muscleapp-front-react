4dfe757dde3550ffe5300ecbc76a3337755e77e9
kilianp07 <kilianp07@icloud.com>
2023-05-30 14:14:55 +0200

    Add login page and weight graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import  Login  from './components/login/login'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ButtonAppBar from './components/navbar/navbar'
+import WeightGraph from './components/weightGraph/graph'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+        <ButtonAppBar />
+        </nav>
+
+        {/* Passez à différentes pages */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<WeightGraph />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
